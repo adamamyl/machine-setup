3c087b894d0abdeeb573ebb3f62ddfe3e92efd1a
Adam McGreggor <adam@adamamyl.com>
2025-11-23 00:27:52 +0000

    Re-write function for things to be done as root/user

--- lib/installers/pseudohome.sh
+++ lib/installers/pseudohome.sh
@@ -3,39 +3,46 @@ set -euo pipefail
 IFS=$'\n\t'
 
 setup_pseudohome() {
-    local user="adam"
-    require_user "$user"
-    add_user_to_group "$user" staff
+  local user="adam"
 
-    local homedir
-    homedir="$(eval echo "~$user")"
-    local ssh_dir="$homedir/.ssh"
-    _cmd "mkdir -p -m 700 $ssh_dir"
-    _cmd "chown $user:$user $ssh_dir"
+  # --- root tasks ---
+  _cmd "require_user '$user'"
+  _cmd "add_user_to_group '$user' staff"
 
-    # Install SSH keys
-    install_ssh_keys "$user" "https://github.com/adamamyl.keys"
+  local homedir
+  homedir="$(eval echo "~$user")"
+  local ssh_dir="$homedir/.ssh"
 
-    # Clone pseudohome repo
-    local repo_url="git@github.com:adamamyl/pseudoadam.git"
-    local dest_dir="$homedir/pseudohome"
+  _cmd "mkdir -p -m 700 '$ssh_dir'"
+  _cmd "chown $user:$user '$ssh_dir'"
 
-    if [[ ! -d "$dest_dir" ]]; then
-        clone_or_update_repo "$repo_url" "$dest_dir"
-        _cmd "chown -R $user:$user $dest_dir"
-        _cmd "chmod -R g+w $dest_dir"
+  # Install SSH keys as root
+  _cmd "install_ssh_keys '$user' 'https://github.com/adamamyl.keys'"
 
-        # Run symlink installer inside venv and sudo
-        if [[ -x "$dest_dir/pseudohome-symlinks" ]]; then
-            info "Running pseudohome symlinks..."
-            sudo -H -u "$user" bash -c "
-                export PATH=\"$VENVDIR/bin:\$PATH\"
-                $dest_dir/pseudohome-symlinks
-            "
-        else
-            warn "Symlink installer not executable"
-        fi
-    else
-        ok "Pseudohome already exists, skipping clone and symlinks"
-    fi
+  # --- user tasks ---
+  local repo_url="git@github.com:adamamyl/pseudoadam.git"
+  local dest_dir="$homedir/pseudohome"
+
+  if [[ ! -d "$dest_dir" ]]; then
+    _cmd "sudo -H -u '$user' bash -c '
+      set -euo pipefail
+      IFS=$'\''\n\t'\''
+      export PATH=\"$VENVDIR/bin:\$PATH\"
+
+      # Clone repository if missing
+      if [[ ! -d \"$dest_dir\" ]]; then
+        git clone \"$repo_url\" \"$dest_dir\"
+      fi
+
+      chown -R $user:$user \"$dest_dir\"
+      chmod -R g+w \"$dest_dir\"
+
+      # Run symlink installer if executable
+      if [[ -x \"$dest_dir/pseudohome-symlinks\" ]]; then
+        \"$dest_dir/pseudohome-symlinks\"
+      fi
+    '"
+  else
+    _cmd "echo 'Pseudohome already exists, skipping clone and symlinks'"
+  fi
 }
